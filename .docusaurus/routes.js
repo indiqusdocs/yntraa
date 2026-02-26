f1b1795e0f0c6614ad8aff6cf676c48a31900bfe
PawanGaur7 <prkumar@yotta.com>
2026-02-26 10:17:26 +0530

    update

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,6 +3,41 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '5ff'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
+    exact: true
+  },
+  {
     path: '/blog',
     component: ComponentCreator('/blog', 'b2f'),
     exact: true
@@ -84,15 +119,15 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', '037'),
+    component: ComponentCreator('/docs', '4a1'),
     routes: [
       {
         path: '/docs',
-        component: ComponentCreator('/docs', 'ec4'),
+        component: ComponentCreator('/docs', 'add'),
         routes: [
           {
             path: '/docs',
-            component: ComponentCreator('/docs', 'c86'),
+            component: ComponentCreator('/docs', '1fb'),
             routes: [
               {
                 path: '/docs/category/about-vpc-instances',
@@ -245,8 +280,8 @@ export default [
                 sidebar: "sidebar1"
               },
               {
-                path: '/docs/category/other-service',
-                component: ComponentCreator('/docs/category/other-service', '3cc'),
+                path: '/docs/category/other-services',
+                component: ComponentCreator('/docs/category/other-services', 'eb7'),
                 exact: true,
                 sidebar: "sidebar1"
               },
