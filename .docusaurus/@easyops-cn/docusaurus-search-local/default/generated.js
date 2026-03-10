ac8531cb36906bceed7f8c7750d592afc66ea379
PawanGaur7 <prkumar@yotta.com>
2026-03-10 13:43:39 +0530

    work update

--- .docusaurus/@easyops-cn/docusaurus-search-local/default/generated.js
+++ .docusaurus/@easyops-cn/docusaurus-search-local/default/generated.js
@@ -3,7 +3,7 @@ export const language = ["en"];
 export const removeDefaultStopWordFilter = false;
 export const removeDefaultStemmer = false;
 export const Mark = null;
-export const searchIndexUrl = "search-index{dir}.json?_=fe33e12a";
+export const searchIndexUrl = "search-index{dir}.json?_=4f648040";
 export const searchResultLimits = 8;
 export const searchResultContextMaxLength = 50;
 export const explicitSearchResultPath = false;
