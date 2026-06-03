dac3877eb0ec97cadae2742475eb97258f6b2460
PawanGaur7 <prkumar@yotta.com>
2026-06-03 11:03:23 +0530

    work update

--- .docusaurus/@easyops-cn/docusaurus-search-local/default/generated.js
+++ .docusaurus/@easyops-cn/docusaurus-search-local/default/generated.js
@@ -3,7 +3,7 @@ export const language = ["en"];
 export const removeDefaultStopWordFilter = false;
 export const removeDefaultStemmer = false;
 export const Mark = null;
-export const searchIndexUrl = "search-index{dir}.json?_=95ab3e28";
+export const searchIndexUrl = "search-index{dir}.json?_=36d494fe";
 export const searchResultLimits = 8;
 export const searchResultContextMaxLength = 50;
 export const explicitSearchResultPath = false;
